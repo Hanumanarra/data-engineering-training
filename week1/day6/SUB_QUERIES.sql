034f099f2039f57051553b0800b95ae5173b1a28
Hanumanarra <151939652+Hanumanarra@users.noreply.github.com>
2026-04-11 21:32:47 +0530

    Update SUB_QUERIES.sql

--- week1/day6/SUB_QUERIES.sql
+++ week1/day6/SUB_QUERIES.sql
@@ -1,5 +1,7 @@
 SUBQUERIES
 
+	-# FROM CLAUSE
+	
 #1 Rank Customers Based on there total Amount of sales
 
 select 
@@ -25,3 +27,20 @@ from
 	avg(amount) over() as Avg_amount
 	from Orders)
 where amount>Avg_amount
+
+-#SELECT CLAUSE
+	
+
+1.show order_id,item,amount and total_sales of products from Orders
+	
+ very Imp Point # in SELECT CLAUSE WE CAN USE ONLY SCALAR SUBQUERYS ONLY
+	
+select 
+	order_id,
+	item,
+	amount,
+	(select count(*) as Total_order from Orders) Total_sales
+from Orders;
+
+-Join Clause
+
