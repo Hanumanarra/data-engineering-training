5cbd2881f214cef0d6b838a2b20503f8e90e11dd
Hanumanarra <151939652+Hanumanarra@users.noreply.github.com>
2026-04-12 19:44:30 +0530

    Update CTE_query.sql

--- week1/day5/CTE_query.sql
+++ week1/day5/CTE_query.sql
@@ -17,3 +17,35 @@ cts.total_amount
 from Customers c
 left join CTE_total_sales cts
 on c.customer_id=cts.customer_id
+
+2.using Multiple CTE
+
+with CTE_total_sales as
+(
+select 
+  customer_id,
+  sum(amount) as total_amount
+from Orders
+group by customer_id
+)
+,CTE_max_item as
+(
+select
+  customer_id,
+  Max(item) as  Max_item
+from Orders
+group by customer_id
+)
+
+select 
+c.customer_id,
+c.first_name,
+c.last_name,
+cts.total_amount,
+cta.max_item
+from Customers c
+left join CTE_total_sales cts
+on c.customer_id=cts.customer_id
+left join CTE_max_item cta
+on c.customer_id=cta.customer_id
+
